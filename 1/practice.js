b16a7d543a1b4c70acc9357e7e0bc68adb3beea1
JK <jk.jonggyuk@gmail.com>
2018-12-21 12:59:31 -0800

    push some chg

--- 1/practice.js
+++ 1/practice.js
@@ -1,4 +1,6 @@
 const title = document.getElementById("title");
 console.log(title);
 title.innerHTML = "now chaged!";
-
+title.style.color = "red";
+console.dir(document);
+document.title = 'Hello from the other side';
